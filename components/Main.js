fab326907c4b97ef30ec45e234b07d6bde49b487
Luis Ortiz <ortizlu@icloud.com>
2018-10-20 15:38:21 -0400

    tried to do something

---
+++ components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const Main = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Reduxstagram</h1>
+      </div>
+    )
+  }
+})
